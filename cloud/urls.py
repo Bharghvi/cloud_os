9e8c5c1158260f4556d9e32dbf9759d0295d5538
Bharghvi <bsprajapati97@gmail.com>
2018-12-02 13:58:45 +0530

    VM instance pages added

--- cloud/urls.py
+++ cloud/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^main/$', views.main, name='main'),
+    url(r'^$', views.home, name='home')
 ]
--- cloud/views.py
+++ cloud/views.py
@@ -1,8 +1,12 @@
-# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'cloud/index.html', )
+
+def main(request):
+    return render(request, 'cloud/main.html', )
+
+def home(request):
+    return render(request, 'cloud/home.html', )
